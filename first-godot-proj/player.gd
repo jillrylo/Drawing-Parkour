c3c861e2157a4e4b3554f8f803f274c69eb143b7
jillrylo <jillycurylee@gmail.com>
2025-11-18 14:41:18 -0500

    girlcanmove (not correctly)

---
+++ first-godot-proj/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed : float = 300.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction : Vector2 = Input.get_vector("left", "right", "jump", "crouch")
+	
+	velocity = direction * speed
+	
+	move_and_slide()
+	pass
