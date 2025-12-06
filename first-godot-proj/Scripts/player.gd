703e97285f60d3f6ba9c64a98665ad29a26215df
Jill Curylo <jillycurylee@gmail.com>
2025-12-05 20:35:37 -0500

    coding camera bounds helppppppppppp

--- first-godot-proj/Scripts/player.gd
+++ first-godot-proj/Scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-
+class_name Player
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -800.0
 
+@onready var _animated_sprite = $AnimatedSprite2D
+@onready var camera_limit_manager: Camera = $Camera
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,24 +15,28 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if not is_on_floor():
+		if velocity.y < 0:
+			_animated_sprite.play("jump")
+		else:
+			_animated_sprite.play("idle")
+			
+	elif direction !=0: 
+		_animated_sprite.play("walk")
+		_animated_sprite.flip_h = direction < 0
+	else:
+		_animated_sprite.play("idle")
 
 
 	move_and_slide()
 	
-
-
-@onready var _animated_sprite = $AnimatedSprite2D
-
-func _process(_delta):
-	if Input.is_action_pressed("ui_right"):
-		_animated_sprite.play("walk")
-	else:
-		_animated_sprite.stop()
+func _ready():
+	camera_limit_manager.player = self
---
+++ first-godot-proj/camera.gd
@@ -0,0 +1,5 @@
+extends Camera2D
+
+class_name Camera
+
+@onready var player: Player = get_parent()
