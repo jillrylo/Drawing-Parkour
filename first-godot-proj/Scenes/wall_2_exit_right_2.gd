44d06b735ff77da247f8498edb746b8f25933040
Jill Curylo <jillycurylee@gmail.com>
2025-12-09 12:30:58 -0500

    code for each level

---
+++ first-godot-proj/Scenes/wall_2_exit_right_2.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+@export var target_spawn:NodePath
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file("res://Scenes/wall_4.tscn")
