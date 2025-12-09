34326c9ec90a04deb45f4b2cd86138657b4eec8d
Eli Patino <phantmssb@gmail.com>
2025-12-09 14:13:09 -0500

    Added jump sound effect and regulated the audio
    Sound design lowkey my passion.

--- first-godot-proj/Scripts/player.gd
+++ first-godot-proj/Scripts/player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
+		$AudioStreamPlayer.play()
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
