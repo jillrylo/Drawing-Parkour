7943aafd64eb7f394b5e09de47a77410c72322fa
Jill Curylo <jillycurylee@gmail.com>
2025-12-09 12:41:09 -0500

    code works for levels

--- first-godot-proj/Scenes/wall_3_exit_right_2.gd
+++ first-godot-proj/Scenes/wall_3_exit_right_2.gd
@@ -8,4 +8,4 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://Scenes/wall_3.tscn")
+		get_tree().change_scene_to_file("res://Scenes/wall_5.tscn")
