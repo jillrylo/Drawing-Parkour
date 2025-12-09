c241b57511de90c55c97f21d9f85e5ada275adfc
Jill Curylo <jillycurylee@gmail.com>
2025-12-09 02:31:44 -0500

    transition workinggg

--- first-godot-proj/Scenes/start_button.gd
+++ first-godot-proj/Scenes/start_button.gd
@@ -2,5 +2,5 @@ extends TextureButton
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/wall_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/wall_2.tscn")
  # Replace with function body.
--- first-godot-proj/Scripts/player.gd
+++ first-godot-proj/Scripts/player.gd
@@ -38,9 +38,3 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-
-
-func _on_interaction_area_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Portal"):
-		position.x = -1133
-		position.y = -809
